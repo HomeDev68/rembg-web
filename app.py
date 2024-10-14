8a3ade148850597725407165ffb81631e5df5881
HomeDev68 <180551684+HomeDev68@users.noreply.github.com>
2024-10-14 23:15:07 +0100

    curl optimization

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_file
 from rembg import remove
 from PIL import Image
 from io import BytesIO
+import os
 
 app = Flask(__name__)
 
@@ -10,18 +11,21 @@ def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             return 'No file uploaded', 400
-        else:
-            file = request.files['file']
-            if file.filename == '':
-                return 'No file selected', 400
-            elif file:
+        file = request.files['file']
+        if file.filename == '':
+            return 'No file selected', 400
+        if file:
+            try:
                 input_image = Image.open(file.stream)
                 output_image = remove(input_image, post_process_mask=True)
                 img_io = BytesIO()
                 output_image.save(img_io, 'PNG')
                 img_io.seek(0)
-                # return send_file(img_io, mimetype='image/png')  # Change download in seperate browser tab
-                return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='_rmbg.png')
+                filename, file_extension = os.path.splitext(file.filename)
+                download_name = filename + '_rmbg.png'
+                return send_file(img_io, mimetype='image/png', as_attachment=True, download_name=download_name)
+            except Exception as e:
+                return f"Error processing image: {e}", 500
     else:
         return render_template('index.html')
 
